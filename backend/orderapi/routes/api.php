aebaf912762863bb9ded92a83a2a409dc0478e70
Daniel Bedoya <95carloshdn@gmail.com>
2024-03-05 12:29:34 -0500

    Servicios p1
    Se crean servicios de causal, observation, type_activity y technician

--- backend/orderapi/routes/api.php
+++ backend/orderapi/routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\CausalController;
+use App\Http\Controllers\ObservationController;
+use App\Http\Controllers\TechnicianController;
+use App\Http\Controllers\TypeActivityController;
+use App\Models\TypeActivity;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +22,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/* Route::prefix('causal')->group(function () {
+    Route::controller(CausalController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::post('/', 'store');
+        Route::get("/{causal}", "show");
+        Route::put('/{causal}', 'update');
+        Route::delete("/{causal}", "destroy");
+    });
+});  */
+
+Route::apiResource('causal', CausalController::class);
+Route::apiResource('observation', ObservationController::class);
+Route::apiResource('technician', TechnicianController::class);
+Route::apiResource('type_activity', TypeActivityController::class);
